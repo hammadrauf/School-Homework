35a0161d2166f23a3634750080564fb566fcf604
hammadrauf <rauf.hammad@gmail.com>
2014-09-18 21:12:56 -0700

    Fixing Tomcat JDBCRealm problem.

--- src/main/java/actions/ActionSupportTimesTable2.java
+++ src/main/java/actions/ActionSupportTimesTable2.java
@@ -126,6 +126,7 @@ public class ActionSupportTimesTable2 extends ActionSupport {
         // step 2
         ServletContext sc = ServletActionContext.getServletContext();
         String FilePath = sc.getRealPath("/");
+        FilePath = "/tmp/";
         Logger.getLogger(actions.ActionSupportTimesTable2.class.getName()).log(Level.INFO, "ActionSupportTimesTable2 > generatePDF > ServletContextPath = " + ((FilePath==null)?"empty":FilePath));
         String outPath = (FilePath==null)?"temp/" + FNAME:FilePath + "temp/" + FNAME;
         String pathForJavaScript = "./" + "temp/" + FNAME;
