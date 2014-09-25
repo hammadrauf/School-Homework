7ec37bd16149a8541ce7bad81a1695778414d702
hammadrauf <rauf.hammad@gmail.com>
2014-09-24 17:34:29 -0700

    Fixing pdf path on openshift problem.

--- src/main/java/actions/ActionSupportTimesTable2.java
+++ src/main/java/actions/ActionSupportTimesTable2.java
@@ -125,7 +125,7 @@ public class ActionSupportTimesTable2 extends ActionSupport {
         // step 2
         ServletContext sc = ServletActionContext.getServletContext();
         String FilePath = sc.getRealPath("/");
-        FilePath = "/var/lib/openshift/54105d5fe0b8cd569a000153/app-deployments/current/repo/target/demo/";
+        FilePath = "/var/lib/openshift/54105d5fe0b8cd569a000153/app-root/runtime/repo/target/demo/";
         String outPath = (FilePath==null)?"temp/" + FNAME:FilePath + "temp/" + FNAME;
         Logger.getLogger(actions.ActionSupportTimesTable2.class.getName()).log(Level.INFO, "ActionSupportTimesTable2 > generatePDF > ServletContextPath (outPath) = " + ((outPath==null)?"empty":outPath));
         String pathForJavaScript = "/" + "temp/" + FNAME;
